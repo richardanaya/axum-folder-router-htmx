5eab3f90ef67c009eb76d62a55295ae39acab463
Richard Anaya <richard.anaya@gmail.com>
2025-04-13 22:10:11 -0700

    remove

--- src/api/items/route.rs
+++
@@ -1,9 +0,0 @@
-use crate::model::Item; // Import the Item struct
-use crate::AppState; // Import AppState and Item from main.rs (or models module if refactored)
-use askama::Template;
-use axum::{
-    extract::State,
-    http::StatusCode,
-    response::{Html, IntoResponse, Response}, // Response is already imported
-};
-use sqlx::PgPool;
